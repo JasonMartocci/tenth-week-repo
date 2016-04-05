ff20c866d8c5f3a235796829c1584f43740c5dfc
Jason Martocci <Jason Martocci>
2016-04-04 20:58:07 -0400

    new classwork

---
+++ 10.1/adele.js
@@ -0,0 +1,9 @@
+// console.log(parseFloat.argv[2] + parseFloat.argv[3]);
+var a = process.argv[2];
+var b = process.argv[3];
+
+if (a == b){
+ 	console.log("They are equal")
+} else{
+	console.log("They are not equal")
+}
